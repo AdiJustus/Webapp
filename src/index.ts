dd9bb6d0f8856aeeebdd6fd795dfa0b6bdb7e4ba
AdiJustus <AdrianJustusFleischer@gmail.com>
2021-04-17 10:24:41 +0200

    Changes to HTML

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-const appear = () => {
-    document.getElementById("clothing1")!.style.display ='block';
-    document.getElementById("clothing2")!.style.display ='none'
-    document.getElementById("clothing3")!.style.display ='none'
-  }

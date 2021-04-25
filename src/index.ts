4ff9cb05c1b131a0b51470fef76a7233c7b2a6cf
AdiJustus <AdrianJustusFleischer@gmail.com>
2021-04-25 18:52:10 +0200

    Added Clothing Selection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
-function ausgabe():void {
+let previewBox = document.getElementById("previewBox") as HTMLInputElement;
+const Kleidung = document.getElementsByClassName("Kleidung");
 
-    console.log("T-Shirt");
+function ausgabe(ArtDerKleidung:string):void {
+    console.log(ArtDerKleidung);
+    if (ArtDerKleidung === 'T-Shirt') {
+        previewBox.innerHTML = '<img src="../Bilder/tshirt.svg" class="Kleidung">';
+        console.log("T-Shirt");
+    }
+    else if (ArtDerKleidung === 'Shirt') {
+        previewBox.innerHTML = '<img src="../Bilder/shirts.svg">';
+        console.log("Shirt");
+    }
+    else if (ArtDerKleidung === 'Hoodie') {
+        previewBox.innerHTML = '<img src="../Bilder/hoodie.svg">';
+        console.log("Hoodie");
+    }
+    else if (ArtDerKleidung === 'Sweater') {
+        previewBox.innerHTML = '<img src="../Bilder/sweater.svg">';
+        console.log("Sweater");
+    }
 }
 
+function addColor() {
+    Kleidung.style.color = document.querySelector("#colorMaker").value;
+}
+
+}

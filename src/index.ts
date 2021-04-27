9742fc86f425492cb25b5a1273cee671642e46d8
AdiJustus <AdrianJustusFleischer@gmail.com>
2021-04-27 22:21:59 +0200

    Color Function added for all clothes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,34 @@
 let previewBox = document.getElementById("previewBox") as HTMLInputElement;
 const Kleidung = document.getElementsByClassName("Kleidung");
 const Tshirt = document.querySelector("#tshirt") as SVGElement;
-const color = document.querySelector("#colorMaker") as HTMLInputElement;
 const colorFields = document.querySelectorAll(".colorField") as unknown as Array<HTMLElement>;
 var kleidung: string;
 
 function changeColor(color:string) {
+
     if (kleidung === 'T-Shirt') {
+
         const tshirt = document.querySelector("#inlinePath") as SVGElement;
         tshirt!.setAttribute("fill", color);
-    } else if (kleidung === 'Shirt') {
+
+    } 
+    else if (kleidung === 'Shirt') {
+
         const shirt = document.querySelector("#inlinePath2") as SVGElement;
         shirt!.setAttribute("fill", color);
+
+    }
+    else if (kleidung === 'Hoodie') {
+
+        const hoodie = document.querySelector("#inlinePath3") as SVGElement;
+        hoodie!.setAttribute("fill", color);
+
+    }
+    else if (kleidung === 'Sweater') {
+
+        const sweater = document.querySelector("#inlinePath4") as SVGElement;
+        sweater!.setAttribute("fill", color);
+
     }
 }
 
@@ -23,29 +40,49 @@ function hideElements() {
     const shirt = document.querySelector("#previewBox2") as HTMLElement;
     shirt!.style.display = "none";
 
+    const hoodie = document.querySelector("#previewBox3") as HTMLElement;
+    hoodie!.style.display = "none";
+
+    const sweater = document.querySelector("#previewBox4") as HTMLElement;
+    sweater!.style.display = "none";
+
 }
 
 function ausgabe(ArtDerKleidung:string):void {
+
     hideElements();
-    console.log(ArtDerKleidung);
+
     kleidung = ArtDerKleidung;
+
     if (ArtDerKleidung === 'T-Shirt') {
+
         const tshirt = document.querySelector("#previewBox") as HTMLElement;
         tshirt!.style.display = "inline";
-        console.log("T-Shirt");
+
     }
+
     else if (ArtDerKleidung === 'Shirt') {
+
         const shirt = document.querySelector("#previewBox2") as HTMLElement;
         shirt!.style.display = "inline";
         console.log("Shirt");
+
     }
+
     else if (ArtDerKleidung === 'Hoodie') {
-        previewBox.innerHTML = '<img src="../Bilder/hoodie.svg">';
+
+        const hoodie = document.querySelector("#previewBox3") as HTMLElement;
+        hoodie!.style.display = "inline";
         console.log("Hoodie");
+
     }
+
     else if (ArtDerKleidung === 'Sweater') {
-        previewBox.innerHTML = '<img src="../Bilder/sweater.svg">';
+
+        const sweater = document.querySelector("#previewBox4") as HTMLElement;
+        sweater!.style.display = "inline";
         console.log("Sweater");
+
     }
 }
 

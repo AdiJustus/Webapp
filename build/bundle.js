cd77568e48ca13ad91907df4adb10658b1ef702d
AdiJustus <AdrianJustusFleischer@gmail.com>
2021-05-04 14:54:54 +0200

    build.js now working

---
+++ build/bundle.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+
+  document.getElementById("previewBox");
+  document.getElementsByClassName("Kleidung");
+  document.getElementsByClassName("sizeSelection");
+  document.querySelectorAll(".colorField");
+  document.getElementById("Warenkorb");
+  document.getElementById("sizeSelector");
+  document.getElementById("materialSelector");
+  document.getElementById("Ergebnis");
+
+}());

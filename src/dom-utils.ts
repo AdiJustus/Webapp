0793891abcfcd0309fc3471794a2a4465e67c3af
AdiJustus <AdrianJustusFleischer@gmail.com>
2021-05-06 14:25:32 +0200

    Small Changes

--- src/dom-utils.ts
+++ src/dom-utils.ts
@@ -1,8 +1,8 @@
 //Outsorcing HTML elemtens
 export let previewPrice = document.getElementById("previewPrice");
-//export const clothing = document.getElementsByClassName("clothing");
-export const size = document.getElementsByClassName("sizeSelection");
-export const colorFields = (document.querySelectorAll(".colorField") as unknown) as Array<HTMLElement>;
+//0export const clothing = document.getElementsByClassName("clothing");
+//export const size = document.getElementsByClassName("sizeSelection");
+//export const colorFields = (document.querySelectorAll(".colorField") as unknown) as Array<HTMLElement>;
 export const shoppingCartList = document.getElementById("shoppingCart");
 export const sizeSelector = document.getElementById("sizeSelector") as HTMLSelectElement;
 export const materialSelector = document.getElementById("materialSelector") as HTMLSelectElement;

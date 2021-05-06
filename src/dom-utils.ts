021c5c305d7a7ad646c726ee6b222496fb1d49f7
AdiJustus <AdrianJustusFleischer@gmail.com>
2021-05-06 17:17:11 +0200

    Updated functions + price in shopping Cart

--- src/dom-utils.ts
+++ src/dom-utils.ts
@@ -1,8 +1,6 @@
 //Outsorcing HTML elemtens
+export let totalPriceText = document.getElementById("shoppingCartPrice");
 export let previewPrice = document.getElementById("previewPrice");
-//0export const clothing = document.getElementsByClassName("clothing");
-//export const size = document.getElementsByClassName("sizeSelection");
-//export const colorFields = (document.querySelectorAll(".colorField") as unknown) as Array<HTMLElement>;
 export const shoppingCartList = document.getElementById("shoppingCart");
 export const sizeSelector = document.getElementById("sizeSelector") as HTMLSelectElement;
 export const materialSelector = document.getElementById("materialSelector") as HTMLSelectElement;
